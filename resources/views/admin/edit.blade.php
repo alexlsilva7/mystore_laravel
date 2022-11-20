92376bafd1d315d002713f622ac88147d479b6c5
alexlsilva7 <alex.lsilva154@gmail.com>
2022-11-20 09:53:11 -0300

    fix old value in edit product page

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -14,25 +14,37 @@
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="name" class="leading-7 text-sm text-gray-600">Nome do Produto</label>
-                                <input value="{{$product->name}}" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input value="{{old('name', $product->name)}}" type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                @error('name')
+                                <span class="text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="price" class="leading-7 text-sm text-gray-600">Preço</label>
-                                <input value="{{$product->price}}" type="text" id="price" name="price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input value="{{old('price', $product->price)}}" type="text" id="price" name="price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                @error('price')
+                                <span class="text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="stock" class="leading-7 text-sm text-gray-600">Estoque</label>
-                                <input value="{{$product->stock}}"  type="text" id="stock" name="stock" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input value="{{old('stock', $product->stock)}}"  type="text" id="stock" name="stock" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                @error('stock')
+                                    <span class="text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="p-2 w-1/2">
                             <div class="relative">
                                 <label for="cover" class="leading-7 text-sm text-gray-600">Imagem de capa</label>
                                 <input type="file" id="cover" name="cover" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                @error('cover')
+                                    <span class="text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         @if($product->cover)
@@ -46,7 +58,15 @@
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="description" class="leading-7 text-sm text-gray-600">Descrição</label>
-                                <textarea id="description" name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{$product->description}}</textarea>
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                    >{{old('description', $product->description)}}</textarea>
+                                @error('description')
+                                    <span class="text-red-500">{{ $message }}</span>
+                                <span class="text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="p-2 w-full">
